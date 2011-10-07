a0d2aac5fd1cdfc198009ac74095ed812ea62a53
RobWaaser <RobWaaser@gmail.com>
2011-10-07 01:05:51 +0000

    Made a test python script

--- testLink.py
+++ testLink.py
@@ -1,1 +1,8 @@
-Testing!
+import socket
+
+print "Hello World! I'm a Python program\n"
+x = socket.getfqdn()
+print "The local host's name is: " + x + "\n"
+print "If you saw this message, the Python interpreter"
+print "is installed and the standard library works!"
+print "Bye now!"
